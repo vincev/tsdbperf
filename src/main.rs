20cd6bc108e922901055c082ff96835f66026b29
Vince <vince.vasta@gmail.com>
2020-11-01 15:58:52 +0000

    Add checks for command line option values

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![deny(rust_2018_idioms)]
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use chrono::Utc;
 use futures::future::try_join_all;
 use log::info;
@@ -11,7 +11,7 @@ mod measurement;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "tsdbperf")]
-struct Opt {
+pub struct Opt {
     /// Number of measurements per device
     #[structopt(long = "measurements", default_value = "100000")]
     num_measurements: u32,
@@ -33,44 +33,34 @@ async fn main() -> Result<()> {
     let log_env = env_logger::Env::default().default_filter_or("info");
     env_logger::from_env(log_env).init();
 
-    let opt = Opt::from_args();
+    let mut opt = Opt::from_args();
 
-    let (num_written, elapsed_secs) = run_workers(&opt).await?;
+    opt.num_workers = opt
+        .num_workers
+        .map(|n| n.max(1))
+        .or_else(|| Some(num_cpus::get() as u32));
 
-    info!(
-        "Wrote {:9} measurements in {:.2} seconds",
-        num_written, elapsed_secs
-    );
+    if opt.num_workers.unwrap() > 64 {
+        Err(anyhow!("Number of workers cannot be grater than 64"))
+    } else if opt.num_devices > 10_000 {
+        Err(anyhow!("Number of devices cannot be grater than 10,000"))
+    } else if opt.dbopts.num_metrics > 100 {
+        Err(anyhow!("Number of metrics cannot be grater than 100"))
+    } else {
+        info!("Number of workers:       {}", opt.num_workers.unwrap());
+        info!("Devices per worker:      {}", opt.num_devices);
+        info!("Metrics per device:      {}", opt.dbopts.num_metrics);
+        info!("Measurements per device: {}", opt.num_measurements);
 
-    let measurements_per_sec = num_written as f64 / elapsed_secs;
-    info!(
-        "Wrote {:9.0} measurements per second",
-        measurements_per_sec.round()
-    );
-
-    info!(
-        "Wrote {:9.0} metrics per second",
-        (measurements_per_sec * opt.dbopts.num_metrics as f64).round()
-    );
-
-    Ok(())
+        run_workers(&opt).await
+    }
 }
 
-async fn run_workers(opt: &Opt) -> Result<(usize, f64)> {
+async fn run_workers(opt: &Opt) -> Result<()> {
     db::init(&opt.dbopts).await?;
-
-    let num_workers = opt
-        .num_workers
-        .unwrap_or_else(|| num_cpus::get() as u32)
-        .max(1);
-    info!("Number of workers:       {}", num_workers);
-    info!("Devices per worker:      {}", opt.num_devices);
-    info!("Metrics per device:      {}", opt.dbopts.num_metrics);
-    info!("Measurements per device: {}", opt.num_measurements);
-
     let start_time = Utc::now();
 
-    let handles = (1..=num_workers)
+    let handles = (1..=opt.num_workers.unwrap())
         .map(|worker_id| {
             let dbopts = opt.dbopts.clone();
             let (num_devices, num_measurements) = (opt.num_devices, opt.num_measurements);
@@ -89,5 +79,21 @@ async fn run_workers(opt: &Opt) -> Result<(usize, f64)> {
 
     let elapsed_secs = (Utc::now() - start_time).num_milliseconds() as f64 / 1000.0;
 
-    Ok((num_written, elapsed_secs))
+    info!(
+        "Wrote {:9} measurements in {:.2} seconds",
+        num_written, elapsed_secs
+    );
+
+    let measurements_per_sec = num_written as f64 / elapsed_secs;
+    info!(
+        "Wrote {:9.0} measurements per second",
+        measurements_per_sec.round()
+    );
+
+    info!(
+        "Wrote {:9.0} metrics per second",
+        (measurements_per_sec * opt.dbopts.num_metrics as f64).round()
+    );
+
+    Ok(())
 }
